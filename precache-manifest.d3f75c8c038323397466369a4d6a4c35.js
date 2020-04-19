4dfc9b2bf6b5ea847e7c090fc26e65d09991c93b
zhang wei <wei.zhang@bespinglobal.cn>
2020-04-20 06:46:31 +0800

    v1 page

---
+++ precache-manifest.d3f75c8c038323397466369a4d6a4c35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565235c17ab21792cfa6436ece856183",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2357d7d28dca1e2eb90",
+    "url": "/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "e41e16ec6b59870a42b8",
+    "url": "/static/css/main.f95fe78c.chunk.css"
+  },
+  {
+    "revision": "a2357d7d28dca1e2eb90",
+    "url": "/static/js/2.ced8a83a.chunk.js"
+  },
+  {
+    "revision": "5d0318134ad40547515e25cd5c65af80",
+    "url": "/static/js/2.ced8a83a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e41e16ec6b59870a42b8",
+    "url": "/static/js/main.7d770fd5.chunk.js"
+  },
+  {
+    "revision": "e9eb442f7528b1835efd",
+    "url": "/static/js/runtime-main.722fb09b.js"
+  }
+]);
